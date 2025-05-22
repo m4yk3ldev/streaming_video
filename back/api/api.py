a1aae38d563c9ac27e7be4f5fa957b1511f9ec69
Maykel <hanibal920915@gmail.com>
2025-05-21 22:58:49 -0300

    testing

--- back/api/api.py
+++ back/api/api.py
@@ -6,10 +6,10 @@ from core.config import settings
 api_router = APIRouter()
 
 # Include routers for different resources
-api_router.include_router(videos.router, prefix="/videos", tags=["videos"])
+api_router.include_router(videos.router, prefix="/videos", tags=["videos"], include_in_schema=False)
 api_router.include_router(
-    subtitles.router, prefix="/subtitles", tags=["subtitles"])
+    subtitles.router, prefix="/subtitles", tags=["subtitles"], include_in_schema=False)
 api_router.include_router(
-    categories.router, prefix="/categories", tags=["categories"])
+    categories.router, prefix="/categories", tags=["categories"] , include_in_schema=False)
 api_router.include_router(
-    playlists.router, prefix="/playlists", tags=["playlists"])
+    playlists.router, prefix="/playlists", tags=["playlists"], include_in_schema=False)
